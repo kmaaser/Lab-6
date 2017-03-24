cfac6bdc90e4c626d055f8600896f39f7b155367
kmaaser <kmaaser@capital.edu>
2017-03-23 20:48:50 -0400

    >=

--- Lab6/Lab6/change.cpp
+++ Lab6/Lab6/change.cpp
@@ -9,23 +9,21 @@ int main() {
 	cout << "Enter amount (0 - 99): ";
 	cin >> amount;
 
-	while (amount > 0) {
-		while (amount > 25) {
-			amount -= 25;
-			quarter++;
-		}
-		while (amount > 10) {
-			amount -= 10;
-			dime++;
-		}
-		while (amount > 5) {
-			amount -= 5;
-			nickle++;
-		}
-		while (amount > 1) {
-			amount -= 1;
-			penny++;
-		}
+	while (amount >= 25) {
+		amount -= 25;
+		quarter++;
+	}
+	while (amount >= 10) {
+		amount -= 10;
+		dime++;
+	}
+	while (amount >= 5) {
+		amount -= 5;
+		nickle++;
+	}
+	while (amount >= 1) {
+		amount -= 1;
+		penny++;
 	}
 
 	return 0;
